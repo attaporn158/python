9d309c151cc75c3fba03c0e9a69a7d9c08979be2
attaporn158 <s6906022610827@email.kmutnb.ac.th>
2026-07-22 02:22:15 +0700

    python3

--- Lecture 03/stringcom.py
+++ Lecture 03/stringcom.py
@@ -2,7 +2,7 @@ string1 = "Mary"
 string2 = "Mark"
 
 if string1 == string2:
-    print(f'"{string}" and "{string2}" are equal.')
+    print(f'"{string1}" and "{string2}" are equal.')
 else:
     print(f'"{string1}" and "{string2}" are not equal.')
     
